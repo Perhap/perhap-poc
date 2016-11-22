be1de4ba4e0f657c4137cd6a9e2e7d491d089fcc
Rob Martin <rob@version2beta.com>
2016-11-21 22:08:35 -0700

    following filter and list domains

--- apps/cassandra/lib/cassandra.ex
+++ apps/cassandra/lib/cassandra.ex
@@ -6,82 +6,129 @@ defmodule Cassandra do
  def init do
     {:ok, client} = :cqerl.get_client({})
     [
-      :cqerl.run_query(client, create_keyspace(keyspace)) |> handle_result,
-      :cqerl.run_query(client, create_events_table(table)) |> handle_result
+      :cqerl.run_query(client, create_keyspace) |> handle_result,
+      :cqerl.run_query(client, create_events_table) |> handle_result,
+      :cqerl.run_query(client, create_entity_index) |> handle_result
     ]
   end
 
   def publish %{domain: domain, entity: entity, type: type, event: event} do
     {:ok, client} = :cqerl.get_client({})
-    :cqerl.run_query(client, insert_query(table, domain, entity, type, event)) |> handle_result
+    :cqerl.run_query(client, insert_query(domain, entity, type, event)) |> handle_result
   end
 
   def query criteria do
     {:ok, client} = :cqerl.get_client({})
-    updated_criteria = make_uuid(criteria)
     :cqerl.run_query(
       client,
       Cqlerl.Structs.cql_query(
-        statement: make_select_statement(table, updated_criteria),
-        values: updated_criteria
+        statement: make_select_statement(criteria),
+        values: make_where(criteria)
       )
     ) |> handle_result
+  end
+
+  def get_domain_keys do
+    {:ok, client} = :cqerl.get_client({})
+    :cqerl.run_query(client, make_domain_keys_query) |> handle_result
+  end
+
+  def get_entity_keys domain do
+    {:ok, client} = :cqerl.get_client({})
+    :cqerl.run_query(client, make_entity_keys_query(domain)) |> handle_result
   end
 
   def keyspace, do: Application.get_env(:cassandra, :keyspace)
   def table, do: Application.get_env(:cassandra, :keyspace) <> "." <> Application.get_env(:cassandra, :table)
 
-  def make_uuid criteria do
-    if Map.has_key?(criteria, :entity) do
-      Map.merge(criteria, %{ entity: :uuid.string_to_uuid(criteria[:entity]) })
-    else criteria
-    end
-  end
-
-  def create_keyspace keyspace do
+  def create_keyspace do
     """
     CREATE KEYSPACE IF NOT EXISTS #{keyspace}\
       WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
     """
   end
 
-  def create_events_table table do
+  def create_events_table do
     """
     CREATE TABLE IF NOT EXISTS #{table}
       ( domain varchar,
-        type varchar,
-        entity uuid,
         event_id timeuuid,
+        entity uuid,
+        type varchar,
         event text,
-        PRIMARY KEY (domain, type, entity, event_id))
-        WITH CLUSTERING ORDER BY (type ASC, entity ASC, event_id DESC);
+        PRIMARY KEY (domain, event_id, entity))
+        WITH CLUSTERING ORDER BY (event_id DESC);
     """
   end
 
-  def insert_query table, domain, entity, type, event do
+  def create_entity_index do
+    """
+    CREATE INDEX IF NOT EXISTS on #{table} (entity);
+    """
+  end
+
+  def create_event_id_index do
+    """
+    CREATE INDEX IF NOT EXISTS on #{table} (event_id);
+    """
+  end
+
+  def insert_query domain, entity, type, event do
     """
     INSERT INTO #{table}(domain, type, entity, event_id, event)
       VALUES ('#{domain}', '#{type}', #{entity}, now(), '#{Poison.encode!(event)}');
     """
   end
 
-  def make_select_statement table, criteria do
+  def make_select_statement criteria do
     "SELECT event_id, domain, type, entity, dateOf(event_id) AS unixtime, event FROM #{table} WHERE " <>
-    ( Enum.map(criteria, fn {k, _} -> "#{k}=?" end) |> Enum.join(" AND ") ) <>
-    ";"
+    ( Enum.map(criteria, fn {k, _} -> 
+      case k do
+        :following -> "event_id > ?"
+        _ -> "#{k} = ?"
+      end
+    end ) |> Enum.join(" AND ") ) <> ";"
+  end
+
+  def make_where criteria do
+    if Map.has_key?(criteria, :following) do
+      Map.merge(criteria, %{ event_id: criteria[:following] })
+    else
+      criteria
+    end
+  end
+
+  def make_domain_keys_query do
+    """
+    SELECT DISTINCT domain FROM #{table};
+    """
+  end
+
+  def make_entity_keys_query domain do
+    """
+    SELECT DISTINCT entity FROM #{table} WHERE domain = '#{domain}';
+    """
+  end
+
+  def handle_result {:ok, {:cql_result, column_spec, _result, _query, _ref} = result } do
+    rows = :cqerl.all_rows result
+    columns = Enum.map column_spec, fn column ->
+      elem column, 3
+    end
+    cond do
+      Enum.all?([:domain, :event_id, :entity, :event, :type], fn column -> Enum.member?(columns, column) end) ->
+        Enum.map rows, fn row -> 
+          Keyword.merge(row, [event_id: :uuid.uuid_to_string(row[:event_id])]) |>
+          Keyword.merge([entity: :uuid.uuid_to_string(row[:entity])]) |>
+          Keyword.merge([event: Poison.decode!(row[:event])])
+        end
+      Enum.member?(columns, :domain) ->
+        rows
+    end
   end
 
   def handle_result {:ok, :void} do
     {:ok, :void}
-  end
-
-  def handle_result {:ok, {:cql_result, _column_spec, _result, _query, _ref} = result } do
-    rows = :cqerl.all_rows result
-    Enum.map rows, fn row ->
-      Keyword.merge(row, [event_id: :uuid.uuid_to_string(row[:event_id])]) |>
-      Keyword.merge([entity: :uuid.uuid_to_string(row[:entity])]) |>
-      Keyword.merge([event: Poison.decode!(row[:event])])
-    end
   end
 
   def handle_result {:ok, result} do
--- apps/cassandra/test/cassandra_test.exs
+++ apps/cassandra/test/cassandra_test.exs
@@ -2,11 +2,33 @@ defmodule CassandraTest do
   use ExUnit.Case
   doctest Cassandra
 
-  test "stores and retrieves an event" do
+  setup_all do
+    Cassandra.init
+    test_domain = :crypto.strong_rand_bytes(24) |> Base.url_encode64 |> binary_part(0, 24)
+
+    on_exit fn ->
+      {:ok, client} = :cqerl.get_client({})
+      :cqerl.send_query(client, "DELETE FROM #{Cassandra.table} WHERE domain = '#{test_domain}';")
+    end
+    [domain: test_domain]
+  end
+
+  test "stores and retrieves an event", context do
     entity = :uuid.uuid_to_string(:uuid.get_v4())
     event = %{"event" => :crypto.strong_rand_bytes(24) |> Base.url_encode64 |> binary_part(0, 24)}
-    {:ok, _} = Cassandra.publish(%{domain: "test", entity: entity, type: "test", event: event})
-    [stored_event] = Cassandra.query(%{domain: "test", type: "test", entity: entity})
+    {:ok, _} = Cassandra.publish(%{domain: context[:domain], entity: entity, type: "test", event: event})
+    [stored_event] = Cassandra.query(%{domain: context[:domain], entity: entity})
     assert event == stored_event[:event]
   end
+
+  test "retrieves events following a given event", context do
+    entity = :uuid.uuid_to_string(:uuid.get_v4())
+    {:ok, _} = Cassandra.publish(%{domain: context[:domain], entity: entity, type: "test", event: %{which_event: "first"}})
+    [stored_event] = Cassandra.query(%{domain: context[:domain], entity: entity})
+    entity2 = :uuid.uuid_to_string(:uuid.get_v4())
+    {:ok, _} = Cassandra.publish(%{domain: context[:domain], entity: entity2, type: "test", event: %{which_event: "second"}})
+    [stored_event2] = Cassandra.query(%{domain: context[:domain], following: stored_event[:event_id]})
+    assert stored_event2[:event]["which_event"] == "second"
+  end
+
 end
