65d6fb2b54b946472677f7d8d48f2583de6057ee
Rob Martin <rob@version2beta.com>
2017-03-09 09:25:04 -0700

    solve warning about deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Perhap.Mixfile do
     [apps_path: "apps",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Dependencies can be Hex packages:
@@ -20,7 +20,7 @@ defmodule Perhap.Mixfile do
   #
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
-  defp deps do
+  defp deps() do
     []
   end
 end
